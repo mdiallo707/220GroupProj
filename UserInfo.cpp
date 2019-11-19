969999704fe3d1e1b43efaf0942a7f312f4a8ff7
emurr567 <47070463+emurr567@users.noreply.github.com>
2019-11-19 11:20:02 -0500

    starting userInfo

--- UserInfo.cpp
+++ UserInfo.cpp
@@ -3,3 +3,4 @@
 //
 
 #include "UserInfo.h"
+
--- UserInfo.h
+++ UserInfo.h
@@ -5,8 +5,11 @@
 #ifndef INC_220GROUPPROJ_USERINFO_H
 #define INC_220GROUPPROJ_USERINFO_H
 
+#include<string>
 
 class UserInfo {
+    std::string userName;
+    int  userAge;
 
 };
 
